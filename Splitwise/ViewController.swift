87926d3e3817946958db00b8630f3a30b7aacf6d
jscheth <sovichea.chet@gmail.com>
2025-04-07 15:52:03 -0700

    Initial Commit

---
+++ Splitwise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Splitwise
+//
+//  Created by Jonathan Cheth on 4/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SplitwiseTests/SplitwiseTests.swift
@@ -0,0 +1,17 @@
+//
+//  SplitwiseTests.swift
+//  SplitwiseTests
+//
+//  Created by Jonathan Cheth on 4/7/25.
+//
+
+import Testing
+@testable import Splitwise
+
+struct SplitwiseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
