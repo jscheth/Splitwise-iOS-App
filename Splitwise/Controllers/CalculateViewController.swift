23c3da62514de1ec246238060c6d01476ce25b0d
jscheth <sovichea.chet@gmail.com>
2025-04-08 13:26:47 -0700

    Create IBActions and IBOutlets

---
+++ Splitwise/Controllers/CalculateViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Splitwise
+//
+//  Created by Jonathan Cheth on 4/7/25.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPercentButton: UIButton!
+    @IBOutlet weak var tenPercentButton: UIButton!
+    @IBOutlet weak var twentyPercentButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func stepValueChanged(_ sender: UIStepper) {
+        
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+    }
+}
+
